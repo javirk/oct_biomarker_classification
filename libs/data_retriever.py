fix loss computation with posweights

libs/data_retriever.py
@@ -152,7 +152,7 @@ class OCTSlicesDataset(Dataset):
 
         self.transform_image = transform_image
 
-        self.weights = self._get_weights_loss()
+        # self.weights = self._get_weights_loss()
         self.posweights = self._get_posweights()
 
     @property
@@ -191,12 +191,12 @@ class OCTSlicesDataset(Dataset):
         sample = {'images': image, 'labels': label, 'paths': self.image_set[idx]} #, 'center': center}
         return sample
 
-    def _get_weights_loss(self):
-        labels_sum = np.sum(self.label_set, axis=0)
-        largest_class = max(labels_sum)
-        weights = largest_class / labels_sum
-        weights = torch.from_numpy(weights)
-        return weights
+    # def _get_weights_loss(self):
+    #     labels_sum = np.sum(self.label_set, axis=0)
+    #     largest_class = max(labels_sum)
+    #     weights = largest_class / labels_sum
+    #     weights = torch.from_numpy(weights)
+    #     return weights
 
     def _get_posweights(self):
         class_counts = np.sum(self.label_set, axis=0)

oct.py
@@ -213,11 +213,11 @@ class OCTMultiLabelDetector:
         lmbda = lambda epoch: 0.99
         scheduler = optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lmbda)
 
-        print(f'Applied weights for training loss will be: {self.trainloader.dataset.weights.numpy()}')
+        print(f'Applied weights for training loss will be: {self.trainloader.dataset.posweights.numpy()}')
 
-        criterion_train = nn.BCEWithLogitsLoss(pos_weight=self.trainloader.dataset.weights.to(self.device)) 
-        criterion_val   = nn.BCEWithLogitsLoss(pos_weight=self.valloader.dataset.dataset.weights.to(self.device)) 
-        criterion_test  = nn.BCEWithLogitsLoss(pos_weight=self.testloader.dataset.dataset.weights.to(self.device)) 
+        criterion_train = nn.BCEWithLogitsLoss(pos_weight=self.trainloader.dataset.posweights.to(self.device)) 
+        criterion_val   = nn.BCEWithLogitsLoss(pos_weight=self.valloader.dataset.dataset.posweights.to(self.device)) 
+        criterion_test  = nn.BCEWithLogitsLoss(pos_weight=self.testloader.dataset.dataset.posweights.to(self.device)) 
 
         best_rocauc_samples = 0.0
         best_rocauc_weighted = 0.0